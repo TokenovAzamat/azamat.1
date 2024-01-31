cc57c812f4cadcaba7772ba95f7313c0b312d646
TokenovAzamat <157094023+TokenovAzamat@users.noreply.github.com>
2024-01-31 12:20:50 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MYAPP',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('MYAPP'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: 200.0,
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Button'),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Button'),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.drive_file_move),
+                label: Text('Data'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Button'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Button'),
+              ),
+              Text(
+                'Hello World! This is a Text Widget.',
+                style: TextStyle(
+                  color: Colors.yellow,
+                  backgroundColor: Colors.black,
+                ),
+              ),
+              IconButton(
+                icon: Icon(Icons.volume_up),
+                onPressed: () {},
+                tooltip: 'Text Speaker',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
